4934b94f84f0bcaa6efc0f0727e47e0a1cb222c2
Sacha Boudjema <sachaboudjema@gmail.com>
2021-07-08 12:49:42 +0200

    Switch to sphynx docstring format

--- openpnpy/messages.py
+++ openpnpy/messages.py
@@ -6,7 +6,8 @@ __all__ = ['backoff', 'device_info']
 
 
 class ContextualTreeBuilder(ElementTree.TreeBuilder):
-    '''Convinience class to allow using contexts with TreeBuilder'''
+    """Convinience class to allow using contexts with TreeBuilder.
+    """
     @contextmanager
     def start(self, tag, attrib={}):
         try:
@@ -16,7 +17,15 @@ class ContextualTreeBuilder(ElementTree.TreeBuilder):
 
 
 def backoff(seconds=0, reason='No Reason'):
-    '''Inform the PnP agent to connect back after some time.'''
+    """Inform the PnP agent to connect back after some time.
+
+    :param seconds: Seconds to wait before connecting back, defaults to 0
+    :type seconds: int, optional
+    :param reason: Reason of the backoff request, defaults to 'No Reason'
+    :type reason: str, optional
+    :return: XML element to be used as a PnP message body
+    :rtype: xml.etree.ElementTree.Element
+    """
     ctb = ContextualTreeBuilder()
     with ctb.start('{urn:cisco:pnp:backoff}request'):
         with ctb.start('backoff'):
@@ -29,10 +38,15 @@ def backoff(seconds=0, reason='No Reason'):
 
 
 def device_info(type='all'):
-    '''Request to retrieve device information from PnP agent.
-    Type can take the following values: image, hardware, filesystem, udi,
-    profile, all
-    '''
+    """Request to retrieve device information from PnP agent.
+    Type can take the following values: .
+
+    :param type: Info sections to be sent, can be 'image', 'hardware', 'filesystem', 
+        'udi', 'profile' or 'all', defaults to 'all'
+    :type type: str, optional
+    :return: XML element to be used as a PnP message body
+    :rtype: xml.etree.ElementTree.Element
+    """
     ctb = ContextualTreeBuilder()
     with ctb.start('{urn:cisco:pnp:device-info}request'):
         with ctb.start('deviceInfo', {'type': type}):
@@ -41,9 +55,12 @@ def device_info(type='all'):
 
 
 def bye():
-    '''Acknowledge the receipt of PnP response and signal the end of the transaction.
+    """Acknowledge the receipt of PnP response and signal the end of the transaction.
     This is applicable only when the transport protocol is HTTP and HTTPS.
-    '''
+
+    :return: XML element to be used as a PnP message body
+    :rtype: xml.etree.ElementTree.Element
+    """
     ctb = ContextualTreeBuilder()
     with ctb.start('{urn:cisco:pnp:work-info}info'):
         with ctb.start('workInfo'):
--- openpnpy/server.py
+++ openpnpy/server.py
@@ -4,8 +4,16 @@ from copy import deepcopy
 
 
 class PnpServer:
+    """Wrapper for of a Flask server implementing PnP protocol endpoints.
+    This class should be derived and handler methods overloaded in to implement
+    behavior.
 
+    seealso:: See https://flask.palletsprojects.com/en/2.0.x/api/#application-object
+        for possible constructor params
+    """
     def __init__(self, *args, **kwargs):
+        """Constructor method
+        """
         self.app = Flask(*args, **kwargs)
         self.app.add_url_rule('/pnp/HELLO', 'hello', self.handle_hello)
         self.app.add_url_rule(
@@ -20,35 +28,56 @@ class PnpServer:
         )
 
     def run(self, *args, **kwargs):
+        """Runs the Flask development server.
+
+        seealso:: See https://flask.palletsprojects.com/en/2.0.x/api/#flask.Flask.run
+            for possible params
+        """
         self.app.run(*args, **kwargs)
 
     def handle_hello(self):
-        '''What to do when a Pnp Agent calls in to the server.
-        Returns an empty http 200 by default.
-        '''
+        """What to do when a PnP Agent calls in to the server.
+        Should not be overridden in most cases.
+
+        :return: HTTP response tuple as specified by Flask, defaults to empty http 
+            200 by default.
+        :rtype: tuple
+        """
         return Response(status=200, headers={})
 
     def handle_work_request(self, work_request):
-        '''What to do when a PnP agent sends a work request.
-        The request passed as argument is of type PnpMessage.
-        Should return a PnP message built using openpnpy.messages functions.
-        Not implemented by default.
-        '''
+        """What to do when a PnP agent sends a work request.
+
+        :param work_request: Work request sent by the PnP qgent
+        :type work_request: openpnpy.server.PnpMessage
+        :return: Body element to be used in the repsonse message
+        :rtype: xml.etree.ElementTree.Element
+        :raises NotImplementedError: To be implemented by user subclass
+
+        note:: The body element can be built using the openpnpy.messages module
+        """
         raise NotImplementedError
 
     def handle_work_response(self, work_response):
-        '''What to do when a PnP agent sends a work response.
-        The response passed as argument is of type PnpMessage.
-        Should return a PnP message built using openpnpy.messages functions.
-        Not implemented by default.
-        '''
+        """What to do when a PnP agent sends a work response.
+
+        :param work_response: Work response sent by the PnP agent
+        :type work_response: openpnpy.server.PnpMessage
+        :return: Body element to be used in the repsonse message
+        :rtype: xml.etree.ElementTree.Element
+        :raises NotImplementedError: To be implemented by user subclass
+
+        note:: The body element can be built using the openpnpy.messages module
+        """
         raise NotImplementedError
-    
+
 
 class PnpMessage:
-    '''PnP XML message
-    The plain ElementTree.Element can be accessed throught self.root.
-    '''
+    """PnP XML message
+
+    :param element: XML parsed message element 
+    :type element: xml.etree.ElementTree.Element
+    """
     def __init__(self, element):
         self.root = element
 
@@ -60,31 +89,36 @@ class PnpMessage:
         return cls(ElementTree.fromstring(xmlstring))
 
     def make_response(self, element):
-        '''Builds a response to this message.
-        Original message is copied and body is replaced by given element.This 
-        allows to preserve session related attributes, such as udi and correlator.
-        Element should be built using openpnpy.messages functions.
-        '''
+        """Builds a response to this message instance preserving session related 
+        attributes.
+
+        :param element: Response message body
+        :type element: xml.etree.ElementTree.Element
+        :return: New PnP message with given body
+        :rtype: openpnpy.server.PnpMessage
+
+        note:: The body element can be built using the openpnpy.messages module
+        """
         response = deepcopy(self)
         response.body = element
         return response
 
     @property
     def udi(self):
-        '''Unique Device Identifier. 
+        """Unique Device Identifier. 
         A built in device id consisting of product id, version id, and the serial
         number. It is mandatory for all the messages that get exchanged between 
         the PnP agent and the PnP server.
-        '''
+        """
         return self.root.get('udi')
 
     @property
     def username(self):
-        '''Login username for the device.
+        """Login username for the device.
         Applicable for all the messages that are sent from the PnP server to the 
         agent. The only exception is the initial exchange when there is no configuration 
         present on the new device.
-        '''
+        """
         self.root.get('username')
 
     @username.setter
@@ -93,39 +127,50 @@ class PnpMessage:
     
     @property
     def password(self):
+        """Login password for the device.
+        Applicable for all the messages that are sent from the PnP server to the 
+        agent. The only exception is the initial exchange when there is no configuration 
+        present on the new device.
+        """
         self.root.get('password')
 
     @password.setter
     def password(self, value):
-        '''Login password for the device.
-        Applicable for all the messages that are sent from the PnP server to the 
-        agent. The only exception is the initial exchange when there is no configuration 
-        present on the new device.
-        '''
         self.root.set('password', value)
 
     @property
     def body(self):
-        '''Body of the PnP message.
+        """Body of the PnP message.
         Can be one of info or request or response messages for various PnP services.
-        '''
+        """
         return self.root[0]
 
     @body.setter
     def body(self, element):
+        """Replaces the message body.
+        The message correlator is apended to the provided body element.
+
+        :param element: Body element to be replaced with
+        :type element: xml.etree.ElementTree.Element
+        
+        note:: The body element can be built using the openpnpy.messages module
+        """
         element.set('correlator', self.correlator)
         self.root[0] = element
 
     @property
     def correlator(self):
-        '''A unique string to match requests and responses between agent and server.'''
+        """A unique string to match requests and responses between agent and server.
+        """
         return self.body.get('correlator')
 
     @property
     def success(self):
-        '''Success state of the last executed work request.
-        None if the message is a work request.
-        '''
+        """Success state of the last executed work request.
+
+        :return: Success state, or None if the message is a work request
+        :rtype: bool, None
+        """
         s = self.body.get('success')
         if s is not None:
             return bool(int(s))
