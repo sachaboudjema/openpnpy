5dda5a59a3650d74431c69bc15d2187f1316a4eb
Sacha Boudjema <sachaboudjema@gmail.com>
2021-07-08 14:02:04 +0200

    Fix doc config

--- docs/conf.py
+++ docs/conf.py
@@ -52,3 +52,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
