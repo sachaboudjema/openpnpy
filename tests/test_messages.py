d2e51ec2b5c806ed24da8f8d2121b07cb492d043
Sacha Boudjema <sachaboudjema@gmail.com>
2021-07-09 16:17:57 +0200

    Fix tests

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,22 +1,14 @@
-import pytest
 from xml.etree import ElementTree
 from openpnpy.messages import bye, backoff
 
 
 class TestBye:
 
-    msg = bye()
-
     def test_return_type(self):
-        assert isinstance(self.msg, ElementTree.Element)
+        assert isinstance(bye(), ElementTree.Element)
 
 
 class TestBackoff:
 
-    msg = backoff(hours=1, minutes=23, seconds=11)
-
     def test_return_type(self):
-        assert isinstance(self.msg, ElementTree.Element)
-    
-    def test_return_type(self):
-        assert isinstance(self.msg, ElementTree.Element)
+        assert isinstance(backoff(), ElementTree.Element)
